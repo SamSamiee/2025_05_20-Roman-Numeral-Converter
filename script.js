00c046d175e808f1b019f0d8c1bffb0979038729
SamSamiee <samsamiee00@gmail.com>
2025-05-20 17:13:09 +0200

    final version

--- script.js
+++ script.js
@@ -19,19 +19,19 @@ const conversion = {
 };
 
 function convertNumber(num, obj) {
-  const keys = [
-    ...Object.keys(obj)
-    .map(Number)
-    .sort((a, b) => b - a),
+	const keys = [
+		...Object.keys(obj)
+			.map(Number)
+			.sort((a, b) => b - a),
 	];
 	return numberFinder(num, keys, obj, "");
 }
 
 function numberFinder(num, arr, obj, str) {
-  if (num === 0) {
-    return str;
+	if (num === 0) {
+		return str;
 	}
-  
+
 	arr = arr.filter((i) => i <= num);
 	console.log(`arr = ${arr}`);
 	str += obj[arr[0]];
@@ -44,25 +44,25 @@ function numberFinder(num, arr, obj, str) {
 console.log(convertNumber(3999, conversion));
 
 btn.addEventListener("click", () => {
-  if (input.value === "") {
-    output.textContent = "";
-    output.style.fontSize = "1.6rem";
-    output.textContent = "Please enter a valid number";
-  } else if (parseInt(input.value) < 1) {
-    output.textContent = "";
-    output.style.fontSize = "1.6rem";
-    output.textContent = "Please enter a number greater than or equal to 1";
-  } else if (parseInt(input.value) >= 4000) {
-    output.textContent = "";
-    output.style.fontSize = "1.6rem";
-    output.textContent = "Please enter a number less than or equal to 3999";
-  } else{
-    output.textContent = convertNumber(parseInt(input.value), conversion)
-  }
+	if (input.value === "") {
+		output.textContent = "";
+		output.style.fontSize = "1.6rem";
+		output.textContent = "Please enter a valid number";
+	} else if (parseInt(input.value) < 1) {
+		output.textContent = "";
+		output.style.fontSize = "1.6rem";
+		output.textContent = "Please enter a number greater than or equal to 1";
+	} else if (parseInt(input.value) >= 4000) {
+		output.textContent = "";
+		output.style.fontSize = "1.6rem";
+		output.textContent = "Please enter a number less than or equal to 3999";
+	} else {
+		output.textContent = convertNumber(parseInt(input.value), conversion);
+	}
 });
 
 input.addEventListener("input", () => {
-  if (input.value.length > 4) {
-    input.value = input.value.slice(0, 4);
-  }
+	if (input.value.length > 4) {
+		input.value = input.value.slice(0, 4);
+	}
 });
